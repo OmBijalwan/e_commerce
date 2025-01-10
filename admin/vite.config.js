541f214146cc5169bbcde9d317dd09b012bce2b7
Om Bijalwan <146260496+OmBijalwan@users.noreply.github.com>
2025-01-10 14:25:00 +0530

    Update vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173, // Use Render's assigned port or default to 5173
+    host: '0.0.0.0' // Expose the server to all network interfaces
+  }
 })
+
