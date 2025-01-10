f7bb0d816e7852bff7b8b2b520e636b400d3e1fa
Om Bijalwan <146260496+OmBijalwan@users.noreply.github.com>
2025-01-10 14:07:44 +0530

    Update ListProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -5,7 +5,8 @@ const ListProduct = () => {
 
   const [allproducts,setAllProducts] = useState([]);
   const fetchInfo = async() =>{
-    await fetch('http://localhost:4000/allproducts')
+   // await fetch('http://localhost:4000/allproducts')
+    await fetch('https://e-commerce-3q3y.onrender.com/allproducts')
     .then((res) =>res.json())
     .then((data)=>{setAllProducts(data)});
   }
@@ -15,7 +16,8 @@ const ListProduct = () => {
   },[])
 
   const remove_product = async(id) =>{
-    await fetch('http://localhost:4000/removeproduct',{
+    // await fetch('http://localhost:4000/removeproduct',{
+      await fetch('https://e-commerce-3q3y.onrender.com/removeproduct',{
       method:'POST',
       headers:{
         Accept:'application/json',
