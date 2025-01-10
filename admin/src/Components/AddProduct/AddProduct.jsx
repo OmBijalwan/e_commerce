b52b5a0aac6e291dab27ffd4487d0547fc270a05
Om Bijalwan <146260496+OmBijalwan@users.noreply.github.com>
2025-01-10 14:05:53 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -72,7 +72,8 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product',image);
 
-        await fetch('http://localhost:4000/upload',{
+       // await fetch('http://localhost:4000/upload',{
+        await fetch('https://e-commerce-3q3y.onrender.com/upload',{
             method:'POST',
             headers:{
                 Accept:'application/json',      
@@ -83,7 +84,8 @@ const AddProduct = () => {
         if(responseData.success){
             product.image = responseData.image_url;
             console.log(product);
-            await fetch('http://localhost:4000/addproduct',{
+        // await fetch('http://localhost:4000/addproduct',{
+            await fetch('https://e-commerce-3q3y.onrender.com/addproduct',{    
                 method:'POST',
                 headers:{
                     Accept:'application/json',
