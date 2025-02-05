05cc8a098a4fed3d6f628badc1b86d2bd73e5c62
Om Bijalwan <146260496+OmBijalwan@users.noreply.github.com>
2025-02-05 17:37:18 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://ombijalwanEcommerce:hK8mDjYLdgtiuUkU@cluster0.pbqvh.mongodb.net/e-commerce", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Database connection error:", error);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+module.exports = connectDB;
 
